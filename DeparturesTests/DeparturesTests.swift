2194a9de4d6ece9ec0e74b0c0f8807a19380fc6d
Fredrik Nannestad <fredrik@digitalfireworks.dk>
2024-11-20 13:18:35 +0100

    Initial Commit

---
+++ DeparturesTests/DeparturesTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeparturesTests.swift
+//  DeparturesTests
+//
+//  Created by Fredrik Nannestad on 20/11/2024.
+//
+
+import Testing
+@testable import Departures
+
+struct DeparturesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
